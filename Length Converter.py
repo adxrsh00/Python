f8af1add937ca272b5f331d323a6d297e8d0e41f
Adarsh Chaurasia <101025480+adxrsh00@users.noreply.github.com>
2022-03-09 03:41:05 +0530

    Create Length Converter.py

---
+++ Length Converter.py
@@ -0,0 +1,10 @@
+cm = 1000; 
+ 
+meter = cm / 100.0; 
+kilometer = cm / 100000.0; 
+ 
+# Driver Code 
+print("Length in meter = " , 
+	 	 	 	meter , "m"); 
+print("Length in Kilometer = ", 
+	 	 	 	kilometer , "km"); 
