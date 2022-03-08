371c56d2d60f1cdab978d7cfa22f508f98b69491
Adarsh Chaurasia <101025480+adxrsh00@users.noreply.github.com>
2022-03-09 03:17:33 +0530

    Create CircleArea.py

---
+++ CircleArea.py
@@ -0,0 +1,6 @@
+def findarea(radius):
+   pi = 22/7
+   area_of_circles = pi * radius * radius
+   print("The area of circle is ",area_of_circles)
+r = int(input("enter Radius of circle : "))
+findarea(r)
