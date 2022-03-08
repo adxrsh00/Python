ef493cbd7a579f639db580e7c283cb56e25590b8
Adarsh Chaurasia <101025480+adxrsh00@users.noreply.github.com>
2022-03-09 03:19:43 +0530

    Create Subjects.py

---
+++ Subjects.py
@@ -0,0 +1,15 @@
+// program to enter marks of five subjects and calculate total, average and percentage. 
+ 
+
+print(“Enter marks of five subjects: “) 
+physics=float(input()) 
+chemistry=float(input()) 
+maths=float(input()) 
+english=float(input()) 
+biology=float(input()) 
+ 
+total = physics + chemistry + maths + english + biology ; average = total/5.0; percentage = (total / 500.0) * 100; 
+ 
+print(“Total marks = “, total) 
+print(“Average marks = “, average) print(“Percentage = “, percentage) 
+ 
